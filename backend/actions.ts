e1c705a25077d93c455ab2df38178c58753c5041
Calvin Lam <calvinlam117@gmail.com>
2024-01-25 16:52:33 +0800

    refactor(db): update db connection

--- backend/actions.ts
+++ backend/actions.ts
@@ -1,13 +1,13 @@
 import db from './database'
 
 const list = async () => {
-    const client = await db().connect();
+    const client = await db();
     const sql = 'SELECT * FROM "Duty" ORDER BY "id" ASC';
     const {rows} = await client.query(sql);
     return rows;
 }
 const read = async (id: string) => {
-    const client = await db().connect();
+    const client = await db();
     const sql = 'SELECT * FROM "Duty" WHERE "id" = $1';
     const values = [id]
     const {rows} = await client.query(sql, values);
@@ -15,7 +15,7 @@ const read = async (id: string) => {
 }
 
 const create = async (name: string) => {
-    const client = await db().connect();
+    const client = await db();
     const sql = 'INSERT INTO "Duty" ("name") VALUES ($1) RETURNING *';
     const values = [name]
     const {rows} = await client.query(sql, values);
@@ -23,7 +23,7 @@ const create = async (name: string) => {
 }
 
 const update = async (id: string, name: string) => {
-    const client = await db().connect();
+    const client = await db();
     const sql = 'UPDATE "Duty" SET "name" = $1 WHERE "id" = $2 RETURNING *';
     const values = [name, id]
     const {rows} = await client.query(sql, values);
@@ -31,7 +31,7 @@ const update = async (id: string, name: string) => {
 }
 
 const del = async (id: string) => {
-    const client = await db().connect();
+    const client = await db();
     const sql = 'DELETE FROM "Duty" WHERE "id" = $1';
     const values = [id]
     const {rows} = await client.query(sql, values);
--- backend/database.ts
+++ backend/database.ts
@@ -1,15 +1,17 @@
-import {Pool} from 'pg';
+import {Pool, PoolClient} from 'pg';
 
-let mainPool: Pool | null = null;
+let mainPool: Promise<PoolClient> | null = null;
 const db = () => {
     if (mainPool) {
         return mainPool;
     }
-    return new Pool({
-        max: 20,
+    const pool = new Pool({
+        max: 2,
         connectionString: 'postgres://db_user:123456!@db:5432/db_name',
         idleTimeoutMillis: 30 * 1000
     });
+    mainPool = pool.connect();
+    return mainPool;
 }
 
 export default db;
